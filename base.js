7e3263c2462844839485173a69c236e28c560227
Sam <samennis127@gmail.com>
2021-12-05 19:50:19 +0000

    Base

--- base.js
+++ base.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let inputs = fs.readFileSync("../input.txt", "utf8").split("\r\n").map(a => a.replace("  ", " "));
+let inputs = fs.readFileSync("input.txt", "utf8").split("\r\n").map(a => a.replace("  ", " "));
 
 for(let i = 0; i < inputs.length; i++) {
     console.log(`Case #${i}: ${solve(inputs[i])}`)
